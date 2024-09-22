e779ff8e3f29798ada0fadbc250c0f3b4e3d18ff
wafaa alomour <wafaa.alomour@gmail.com>
2024-09-22 14:30:54 +0300

    excersies on array and list in c#

---
+++ BusRoute.cs
@@ -0,0 +1,22 @@
+﻿namespace ArrayList;
+
+public class BusRoute
+{
+    public int Number { get; set; }
+    public string Origin { get; set; }
+    public string Destination { get; set; }
+
+
+    public BusRoute(int number, string origin, string destination)
+    {
+        Number = number;
+        Origin = origin;
+        Destination = destination;
+        
+    }
+
+    public override string ToString()
+    {
+        return $"Number: {Number}, Origin: {Origin}, Destination: {Destination}";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ArrayList;
+
+class Program
+{
+ public static void Main(string[] args)
+ {
+  // BusRoute B1 = new BusRoute(10,"new","york");
+  // BusRoute B2 = new BusRoute(20,"new2","york2");
+  //
+  // BusRoute[] routes = [B1, B2];
+  // foreach (BusRoute route in routes)
+  // {
+  //  Console.WriteLine(route);
+  // }
+  
+  List<BusRoute> routes = new List<BusRoute>();
+  
+  routes.Add(new BusRoute(10,"new","yorkKKKK"));
+  routes.Add(new BusRoute(20,"new","londonNNNNN"));
+  Console.WriteLine($"The type of the routes list is: {routes.GetType()}");
+
+  foreach (BusRoute route in routes )
+  {
+   Console.WriteLine(route);
+   Console.WriteLine($"The type of route is: {route.GetType()}");
+
+   
+  }
+  
+  
+
+
+  
+ }
+
+}
